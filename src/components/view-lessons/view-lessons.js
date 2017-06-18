796d0befd7df4db47dbcb79b13c9a6010079b3d0
Fereshta <pereshta.hadji@gmx.de>
2017-06-18 11:03:57 +0200

    changed view-lessons

--- src/components/view-lessons/view-lessons.js
+++ src/components/view-lessons/view-lessons.js
@@ -5,5 +5,47 @@ import angular from 'angular';
 import ViewLessonsComponent from './view-lessons.component';
 
 
+
+angular.module('tabsDemoDynamicHeight', ['ngMaterial']);
+
+
 export default angular.module('ViewLessons', [])
     .component(ViewLessonsComponent.name, new ViewLessonsComponent);
+
+
+
+
+// function to set the default data
+/*$scope.reset = function() {
+
+    fb.$set({
+        monday: {
+            name: 'Monday',
+            slots: {
+                '0900': {
+                    time: '9:00am',
+                    booked: false
+                },
+                '0110': {
+                    time: '11:00am',
+                    booked: false
+                }
+            }
+        },
+        tuesday: {
+            name: 'Tuesday',
+            slots: {
+                '0900': {
+                    time: '9:00am',
+                    booked: false
+                },
+                '0110': {
+                    time: '11:00am',
+                    booked: false
+                }
+            }
+        }
+    });
+
+};*/
+
